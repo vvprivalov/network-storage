e77ebf9a1802b75220fbd604dd5a6155887491be
vvprivalov <vv.privalov@yandex.ru>
2022-04-26 10:52:10 +0300

    Стартуем сетевое хранилище.

---
+++ client/src/main/java/ru/gb/storage/client/Client.java
@@ -0,0 +1,15 @@
+package ru.gb.storage.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
---
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -0,0 +1,8 @@
+package ru.gb.storage.server;
+
+public class Server {
+    public static void main(String[] args) {
+
+        System.out.println("Server started");
+    }
+}
