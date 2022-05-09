bf88f450f91ec80fdbe76cab5fc6342d53040cd4
vvprivalov <vv.privalov@yandex.ru>
2022-05-10 01:00:43 +0300

    Добавил вход в основное окно при регистрации нового пользователя.

--- client/src/main/java/ru/gb/storage/client/FirstClientHandler.java
+++ client/src/main/java/ru/gb/storage/client/FirstClientHandler.java
@@ -6,21 +6,17 @@ import javafx.application.Platform;
 import ru.gb.storage.commons.message.Message;
 import ru.gb.storage.commons.message.SignAnswer;
 import ru.gb.storage.commons.message.TextMessage;
-
 import java.io.IOException;
 
-
 public class FirstClientHandler extends SimpleChannelInboundHandler<Message> {
-
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Message message) {
-        // Получение от сервера текстового сообщения
+        // Получение от сервера текстового сообщения для вывода пользователю
         if (message instanceof TextMessage) {
             TextMessage msg = (TextMessage) message;
             Platform.runLater(() -> Client.startController.lblMessage.setText(msg.getText()));
         }
-
-        // Получение от сервера сообщения об успешности авторизации
+        // Получение от сервера сообщения об успешности авторизации и регистрации
         if (message instanceof SignAnswer) {
             SignAnswer answer = (SignAnswer) message;
             if (answer.isbAnswer()) {
